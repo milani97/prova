1e2080b0befa0595dc3d04a3f6c010c0ca0a35c2
milani97 <felipe.s.milani@hotmail.com>
2021-10-01 21:46:14 -0300

    third commit

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const gerar = require('./random.js')
 
-router.get('/random', function(req, res){
+router.get('/api/lugar/random', function(req, res){
     const lugar = gerar()
     res.json(lugar)
 })
